734338df4590c7933b7d887f4742ecb44cbb15ab
Cameron Greenlee <greenlee.cameron319@gmail.com>
2022-09-23 12:41:49 -0400

    finished layout and design

--- main.js
+++ main.js
@@ -11,23 +11,27 @@ function buildDirectory(customerArray) {
         let picture = document.createElement('img');
         customerDiv.appendChild(picture);
         picture.src = customer.picture.large;
-        picture.classList.add('images')
+        picture.classList.add('pictures');
         
 
         let name = document.createElement('p');
         name.innerText = `${capitalizeFirstLetter(customer.name.title)} ${capitalizeFirstLetter(customer.name.first)} ${capitalizeFirstLetter(customer.name.last)}`;
         customerDiv.appendChild(name);
+        name.classList.add('name')
 
         let email = document.createElement('p');
         email.innerText = `${customer.email}`;
         customerDiv.appendChild(email);
+        email.classList.add('email')
 
         let adressOne = document.createElement('p');
         adressOne.innerText = `${customer.location.street.number} ${customer.location.street.name}`;
         customerDiv.appendChild(adressOne);
+
         let adressTwo = document.createElement('p');
         adressTwo.innerText = `${customer.location.city}, ${customer.location.state} ${customer.location.postcode}`;
         customerDiv.appendChild(adressTwo);
+        adressTwo.classList.add('adressTwo')
 
 
         let dateOfBirth = document.createElement('p');
@@ -37,7 +41,7 @@ function buildDirectory(customerArray) {
         let registered = document.createElement('p');
         registered.innerText = `Customer since: ${moment(customer.registered.date).format('MMM Do YYYY')}`;
         customerDiv.appendChild(registered);
-        
+
         customerContainer.appendChild(customerDiv);
     }
 }
